12f7209f8951c68379d9f13f264abc3319062a0e
arielgreiner <ariel.kiara@gmail.com>
2024-02-11 16:19:09 -0500

    Updated full class code

--- DalhousieENVS2100_IntrotoR/Dalhousie_ENVS2100_IntrotoR_Full.R
+++ DalhousieENVS2100_IntrotoR/Dalhousie_ENVS2100_IntrotoR_Full.R
@@ -52,17 +52,17 @@ ggplot(aes(x = Sepal.Length, y = Sepal.Width, col = Species), data = iris)+ #add
 load(file = "~/Dropbox/Github/TeachingMaterials/DalhousieENVS2100_IntrotoR/avgdata_ENVS2100_IntrotoR.RData") #it's called 'avgdata'
 
 #Look at the Data
-View(avgdata) 
-head(avgdata)
-summary(avgdata) 
-str(avgdata)
+View(Data) 
+head(Data)
+summary(Data) 
+str(Data)
 
 #Data Description
 #ADD DESCRIPTION OF THE DATA HERE
 
 
 #show them how to code a simple plot of the data
-plot(x = avgdata$Date[avgdata$Site == "LTER 1"], y = avgdata$HydroCoral[avgdata$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover (Stress Tol)", main = "LTER 1")
+plot(x = Data$Date[Data$Site == "LTER 1"], y = Data$HydroCoral[Data$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover (Stress Tol)", main = "LTER 1")
 
 #ADD MORE PLOTS HERE
 
@@ -87,41 +87,41 @@ plot(x = avgdata$Date[avgdata$Site == "LTER 1"], y = avgdata$HydroCoral[avgdata$
 #some other plots they could make
 
 ##Basic Scatter plot - Shows how hard coral cover changes over time in LTER 1
-#try changing the site by changing "LTER 1" to another LTER or changing the benthic type by changing "HydroCoral" to "HardCoral_StressTol" or some other benthic type after the '$' sign in the response variable (y) 
-plot(x = avgdata$Date[avgdata$Site == "LTER 1"], y = avgdata$HydroCoral[avgdata$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover (Stress Tol)", main = "LTER 1")
+#try changing the site by changing "LTER 1" to another LTER or changing the benthic type by changing "HardCoral" to "Macroalgae" or some other benthic type after the '$' sign in the response variable (y) 
+plot(x = Data$Date[Data$Site == "LTER 1"], y = Data$HardCoral[Data$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover", main = "LTER 1")
 
 #same plot as above, with a line marking 30% cover
-plot(x = avgdata$Date[avgdata$Site == "LTER 1"], y = avgdata$HydroCoral[avgdata$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover (Stress Tol)", main = "LTER 1", ylim=c(0,1))
+plot(x = Data$Date[Data$Site == "LTER 1"], y = Data$HardCoral[Data$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover", main = "LTER 1", ylim=c(0,1))
 abline(h=0.3, col = "red") 
 
 #can also plot another benthic variable on the same plot using points()
-plot(x = avgdata$Date[avgdata$Site == "LTER 1"], y = avgdata$HydroCoral[avgdata$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover (Stress Tol)", main = "LTER 1", ylim=c(0,1))
-points(x = avgdata$Date[avgdata$Site == "LTER 1"], y = avgdata$Macroalgae[avgdata$Site == "LTER 1"], pch = 20, col = "blue")
+plot(x = Data$Date[Data$Site == "LTER 1"], y = Data$HydroCoral[Data$Site == "LTER 1"], pch = 20, xlab = "Year", ylab = "Hard Coral Cover", main = "LTER 1", ylim=c(0,1))
+points(x = Data$Date[Data$Site == "LTER 1"], y = Data$Macroalgae[Data$Site == "LTER 1"], pch = 20, col = "blue")
 abline(h=0.3, col = "red") 
 #NOTE TO ARIEL: ADD LEGEND HERE
 
 ##Basic Scatter plot - Shows how hard coral cover varies across space in 2005
-#try changing the year by changing 2005 to another year or changing the benthic type by changing "HardCoral_StressTol" to "Sand" or some other benthic type after the '$' sign in the response variable (y) 
+#try changing the year by changing 2005 to another year or changing the benthic type by changing "HardCoral" to "Sand" or some other benthic type after the '$' sign in the response variable (y) 
 #as.factor() changes that vector into a factor variable, enabling it to be plotted along the x-axis since factor variables are ordered character variables  
-plot(x = as.factor(avgdata$Site[avgdata$Date == 2005]), y = avgdata$HardCoral_StressTol[avgdata$Date == 2005], xlab = "Site", ylab = "Hard Coral Cover (Stress Tol)", main = "2005")
+plot(x = as.factor(Data$Site[Data$Date == 2005]), y = Data$HardCoral[Data$Date == 2005], xlab = "Site", ylab = "Hard Coral Cover", main = "2005")
 
 #GGPLOTS
 
 ##Basic Scatter plot - Shows how hard coral cover changes over time in LTER 1
-#try changing the site by changing "LTER 1" to another LTER or changing the benthic type by changing "y = HydroCoral" to "y = HardCoral_StressTol" or some other benthic type 
-ggplot(aes(x = Date, y = HydroCoral), data = avgdata[avgdata$Site == "LTER 1",])+
+#try changing the site by changing "LTER 1" to another LTER or changing the benthic type by changing "y = HardCoral" to "y = Macroalgae" or some other benthic type 
+ggplot(aes(x = Date, y = HardCoral), data = Data[Data$Site == "LTER 1",])+
   ggtitle("LTER 1")+ #add '+' to indicate that you're adding another element to the plot
   geom_point()
 
 #same plot as above, with a line marking 30% cover
 #couldn't remember how to do this, so looked up 'add horizontal line to ggplot' and found http://www.sthda.com/english/wiki/ggplot2-add-straight-lines-to-a-plot-horizontal-vertical-and-regression-lines 
-ggplot(aes(x = Date, y = HydroCoral), data = avgdata[avgdata$Site == "LTER 1",])+
+ggplot(aes(x = Date, y = HardCoral), data = Data[Data$Site == "LTER 1",])+
   ggtitle("LTER 1")+ #add '+' to indicate that you're adding another element to the plot
   geom_point()+
   geom_hline(yintercept = 0.3, col = "red")
 
 #can also plot another benthic variable on the same plot by adding another geom_point() line
-ggplot(aes(x = Date, y = HydroCoral), data = avgdata[avgdata$Site == "LTER 1",])+
+ggplot(aes(x = Date, y = HardCoral), data = Data[Data$Site == "LTER 1",])+
   ggtitle("LTER 1")+ #add '+' to indicate that you're adding another element to the plot
   geom_point()+
   geom_point(aes(x = Date, y = Macroalgae), col = "blue")+
@@ -134,13 +134,13 @@ library(ggplot2)
 #this plot is a bit more advanced, but you can still play around with it by changing which benthic type determines the colouration by changing 'colour' of geom_point below
 
 #correct the longitude because the map is from 0 -> 360 and the longitude is currently ~-149
-avgdata$Longitude_corrected <- avgdata$Longitude +360
+Data$Longitude_corrected <- Data$Longitude +360
 
 worldmap <- map_data ("world", wrap = c(0, 360))
 ggplot(aes(x = long, y = lat), data = worldmap) + 
   geom_polygon(aes(group = group), fill="gainsboro", colour = "gainsboro") +
   xlab("Longitude") + ylab("Latitude")+ 
-  geom_point(data = avgdata[avgdata$Date == 2005,], aes(x = Longitude_corrected, y = Latitude, colour = HydroCoral))+ #, colour = cl.c
+  geom_point(data = Data[Data$Date == 2005,], aes(x = Longitude_corrected, y = Latitude, colour = HardCoral))+ #, colour = cl.c
   coord_cartesian(xlim = c(209,212), ylim = c(-17,-18)) +
   theme_bw()
 
